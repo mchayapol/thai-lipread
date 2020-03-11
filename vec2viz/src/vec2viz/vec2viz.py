3db92b858ed3116f76a4a88e47031ec08b5eb121
Chayapol Moemeng <mchayapol@gmail.com>
2020-03-11 08:32:13 +0700

    WIP vec2viz

--- vec2viz/src/vec2viz/vec2viz.py
+++ vec2viz/src/vec2viz/vec2viz.py
@@ -31,18 +31,126 @@ def fib(n):
     a, b = b, a+b
   return a
 
-def vec2viz(vector_file):
+def find_center(data):
+  """
+    Find center point of the clip
+    RETURN
+    ======
+    a tuple of (c,d)
+    c : center coordinate (x,y)
+    d : data with frame's center
+
+  """
+  sumX = sumY = 0
+  n = len(data)
+  datac = []
+  for f in data:
+    f = f.copy()
+    points = f["top_lip"] + f["bottom_lip"]
+    x1 = x2 = points[0][0]
+    y1 = y2 = points[0][1]
+    for p in points:
+      x,y = p
+      x1 = min(x1,x)
+      x2 = max(x2,x)
+      y1 = min(y1,y)
+      y2 = max(y2,y)
+    f_center = ((x1+x2)/2, (y1+y2)/2)
+    f["center"] = f_center
+    datac.append(f) 
+    sumX += f_center[0]
+    sumY += f_center[1]
+
+  c_center = (int(sumX/n),int(sumY/n))
+  return (c_center,datac)
+
+def shift_center(data,center):
+  """
+  Parameters
+  ==========
+  data is a list of frame with property "center"
+
+  Return
+  ======
+  datas : shifted frames
+  """
+  cX,cY = center
+  datas = []
+  for f in data:
+    f = f.copy()  # Important
+    x,y = f["center"]
+    dX = int(x - cX)
+    dY = int(y - cY)
+    print(dX,dY)
+    # Shift every point with dX, dY
+    top_lip = []
+    for p in f["top_lip"]:
+      x,y = p
+      x += dX
+      y += dY
+      top_lip.append((x,y))  
+
+    bottom_lip = []
+    for p in f["bottom_lip"]:
+      x,y = p
+      x += dX
+      y += dY
+      bottom_lip.append((x,y))
+
+    # print("Shifted----\n\t{}\n\t{}".format(f["top_lip"],top_lip))
+    f["top_lip"] = top_lip
+    f["bottom_lip"] = bottom_lip
+    datas.append(f)
+
+  return datas
+
+def stabilize1(data):  
+  """
+  Stabilize method 1
+  use four corners for ROI to find center point and transform all frames using difference.
+  """
+  c_center, datac = find_center(data)
+  print("Clip center: {}".format(c_center))
+  # print(datac[0]['center'])
+
+  datas = shift_center(datac,c_center)
+
+  print("Origin\n",data[0])
+  print("Origin with center\n",datac[0])
+  print("Shifted\n",datas[0])
+  return datas
+
+def stabilize2(data):
+  """
+  Use the left and right most edge of lips to find the center
+  """
+  datas = []
+  return datas
+
+def vec2viz(vector_file,shake_threshold=10):
+  """
+  shake_threshold is used to detemrine the distance to be ignored for detection.
+  threshold in PERCENTAGE
+  """
   _logger.info("Input file: {}".format(vector_file))
   import json
   with open(vector_file) as json_file:
     data = json.load(json_file)
+    
     data.sort(key=lambda x : x["frame_id"])
-    for f in data:
-      if f["frame_id"] == 0:
-        continue
-      # print("top_lip",len(f["top_lip"]))  >>>> 12
-      # print("bottom_lip",len(f["bottom_lip"])) >>>> 12
+    
+    datas = stabilize1(data)
+
+
+    # for f in datac:
+    #   if f["frame_id"] == 0:
+    #     continue
+      # print("top_lip",len(f["top_lip"]))  #>>>> 12
+      # print("bottom_lip",len(f["bottom_lip"])) #>>>> 12
       # TODO How to evaluate direction? angle of 45' ?
+      
+      # Every frame will shift to this center
+      
       # for k in f.keys():
       #   print(k,f[k])
 
